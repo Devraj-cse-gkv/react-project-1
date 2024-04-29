a178c3890baf8ac3c2d1b7b2f70e9ce67fe83cfd
Devraj <dev.raj.697492@gmail.com>
2024-04-29 21:50:20 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import { NavBar, Sidebar, Footer, Homepage} from './js/temp';
 import reportWebVitals from './reportWebVitals';
 
