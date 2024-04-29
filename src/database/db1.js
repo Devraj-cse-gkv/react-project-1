c3f6203f564716729e7143bc52e5b8217800c667
Devraj <dev.raj.697492@gmail.com>
2024-04-29 21:43:33 +0530

    push

---
+++ src/database/db1.js
@@ -0,0 +1,256 @@
+const links =
+{
+  navlinks: [
+    {
+      name: "Home",
+      url: "homepage"
+    },
+    {
+      name: "Tournaments",
+      url: "tournament"
+    },
+    {
+      name: "Blogs",
+      url: "blogpage"
+    },
+    {
+      name: "Login",
+      url: "login"
+    }
+  ],
+  footer: [
+    {
+      heading: 'Learn More',
+      id: 'footer1',
+      urls: [
+        {
+          name: 'link11',
+          url: 'link11'
+        },
+        {
+          name: 'link12',
+          url: 'link12'
+        },
+        {
+          name: 'link13',
+          url: 'link13'
+        },
+        {
+          name: 'link14',
+          url: 'link14'
+        }
+      ]
+    },
+    {
+      heading: 'services',
+      id: 'footer2',
+      urls: [
+        {
+          name: 'link21',
+          url: 'link21'
+        },
+        {
+          name: 'link22',
+          url: 'link22'
+        },
+        {
+          name: 'link23',
+          url: 'link23'
+        },
+        {
+          name: 'link24',
+          url: 'link24'
+        },
+        {
+          name: 'link25',
+          url: 'link25'
+        },
+        {
+          name: 'link26',
+          url: 'link26'
+        }
+      ]
+    },
+    {
+      heading: 'The industry',
+      id: 'footer3',
+      urls: [
+        {
+          name: 'link31',
+          url: 'link31'
+        },
+        {
+          name: 'link32',
+          url: 'link32'
+        },
+        {
+          name: 'link33',
+          url: 'link33'
+        },
+        {
+          name: 'link34',
+          url: 'link34'
+        }
+      ]
+    },
+    {
+      heading: 'Public Relations',
+      id: 'footer4',
+      urls: [
+        {
+          name: 'link41',
+          url: 'link41'
+        },
+        {
+          name: 'link42',
+          url: 'link42'
+        },
+        {
+          name: 'link43',
+          url: 'link43'
+        },
+        {
+          name: 'link44',
+          url: 'link44'
+        }
+      ]
+
+    },
+    {
+      heading: 'heading5',
+      id: 'footer5',
+      urls: [
+        {
+          name: 'link51',
+          url: 'link51'
+        },
+        {
+          name: 'link52',
+          url: 'link52'
+        },
+        {
+          name: 'link53',
+          url: 'link53'
+        },
+        {
+          name: 'link54',
+          url: 'link54'
+        }
+      ]
+
+    },
+  ]
+};
+const admin =
+{
+  name: 'Devraj',
+  add1: 'Binary Center',
+  add2: 'Chowk road Dumraon',
+  add3: 'Buxar, Bihar, 802119',
+  phone: '+916291388958',
+  email: 'devrajiit46@gmail.com',
+  facebook: 'facebook',
+  instagram: 'instagram',
+  linkedin: 'linkedin',
+  pinterest: 'pinterest',
+  twitter: 'twitter',
+  youtube: 'youtube',
+  github: 'github',
+  about: ' am a SDE.'
+};
+const events = [
+  {
+    name: "Tournament 1",
+    image: "./hero-egame.jpg",
+    price: "$10",
+    schedule: "May 10, 2024",
+    mode: "Single Elimination",
+    status: "Coming soon"
+  },
+  {
+    name: "Tournament 2",
+    image: "/hero-egame.jpg",
+    price: "$20",
+    schedule: "June 5, 2024",
+    mode: "Round Robin",
+    status: "Live"
+  },
+  {
+    name: "Tournament 3",
+    image: "/hero-egame.jpg",
+    price: "Free",
+    schedule: "July 20, 2024",
+    mode: "Double Elimination",
+    status: "Analyzing"
+  },{
+    name: "Tournament 1",
+    image: "./hero-egame.jpg",
+    price: "$10",
+    schedule: "May 10, 2024",
+    mode: "Single Elimination",
+    status: "Coming soon"
+  },
+  {
+    name: "Tournament 2",
+    image: "/hero-egame.jpg",
+    price: "$20",
+    schedule: "June 5, 2024",
+    mode: "Round Robin",
+    status: "Live"
+  },
+  {
+    name: "Tournament 3",
+    image: "/hero-egame.jpg",
+    price: "Free",
+    schedule: "July 20, 2024",
+    mode: "Double Elimination",
+    status: "Analyzing"
+  },{
+    name: "Tournament 1",
+    image: "./hero-egame.jpg",
+    price: "$10",
+    schedule: "May 10, 2024",
+    mode: "Single Elimination",
+    status: "Coming soon"
+  },
+  {
+    name: "Tournament 2",
+    image: "/hero-egame.jpg",
+    price: "$20",
+    schedule: "June 5, 2024",
+    mode: "Round Robin",
+    status: "Live"
+  },
+  {
+    name: "Tournament 3",
+    image: "/hero-egame.jpg",
+    price: "Free",
+    schedule: "July 20, 2024",
+    mode: "Double Elimination",
+    status: "Analyzing"
+  },{
+    name: "Tournament 1",
+    image: "./hero-egame.jpg",
+    price: "$10",
+    schedule: "May 10, 2024",
+    mode: "Single Elimination",
+    status: "Coming soon"
+  },
+  {
+    name: "Tournament 2",
+    image: "/hero-egame.jpg",
+    price: "$20",
+    schedule: "June 5, 2024",
+    mode: "Round Robin",
+    status: "Live"
+  },
+  {
+    name: "Tournament 3",
+    image: "/hero-egame.jpg",
+    price: "Free",
+    schedule: "July 20, 2024",
+    mode: "Double Elimination",
+    status: "Analyzing"
+  }
+];
+export { links, admin, events }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { NavBar, Sidebar, Footer, Homepage} from './js/temp';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function loadTemp() {
+  const nav = ReactDOM.createRoot(document.querySelector('nav'));
+  nav.render(
+    <React.StrictMode>
+      <NavBar />
+    </React.StrictMode>
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  const sidebar = ReactDOM.createRoot(document.getElementById('sidebar'));
+  sidebar.render(
+    <React.StrictMode>
+      <Sidebar />
+    </React.StrictMode>
+  );
+
+  const footer = ReactDOM.createRoot(document.getElementById('footer'));
+  footer.render(
+    <React.StrictMode>
+      <Footer />
+    </React.StrictMode>
+  );
+}
+function homeLoad(){
+  const Home = ReactDOM.createRoot(document.getElementById('main'));
+  Home.render(
+    <React.StrictMode>
+      <Homepage />
+    </React.StrictMode>
+  );
+}
+window.onload = () => {
+  switch(window.location.pathname){
+    case '/' : loadTemp()
+    homeLoad();
+    break;
+    default: alert('404 Error');
+    break;
+  }
+  
+};
+
 reportWebVitals();
